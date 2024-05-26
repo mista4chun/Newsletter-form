72baea93fcca4065cd440b297cb55dc4e08cc101
Fortune Samson <mr4chun4u@gmail.com>
2024-05-26 12:32:32 +0100

    Border now shows error state

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     event.preventDefault();
 
     // Clear previous error states
-    emailInput.classList.remove('border-red-500');
+    emailInput.classList.remove('border-Tomato');
     emailError.classList.add('hidden');
 
     const email = emailInput.value.trim();
@@ -21,10 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (!emailPattern.test(email)) {
       showError(emailInput, emailError, 'valid email required');
     } else {
+      emailInput.removeAttribute('aria-invalid');
+      emailInput.focus()
+
       // Show success message
       successText.innerHTML = `  A confirmation email has been sent to <strong>${email}</strong>. Please open it and click the button inside  to confirm your subscription `;
       successMessage.classList.remove('hidden');
-
       form.classList.add('hidden');
     }
   });
@@ -36,10 +38,11 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   function showError(input, errorElement, message) {
-    input.classList.add('border-red-500');
+    input.classList.add('border-Tomato');
     input.classList.remove('border-gray-300');
     errorElement.textContent = message;
     errorElement.classList.remove('hidden');
+    input.setAttribute('aria-invalid', 'true');
     input.focus();
   }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.{html,js}'],
   screens: {
     sm: '480px',
     md: '768px',
